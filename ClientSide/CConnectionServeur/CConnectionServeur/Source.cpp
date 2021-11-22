3544b14290363389333cbca0d2823ee2ae7794b1
JMLF <49654973+JMLF@users.noreply.github.com>
2021-11-22 14:45:58 +0100

    utilisation cli de la class connection serveur

--- ClientSide/CConnectionServeur/CConnectionServeur/Source.cpp
+++ ClientSide/CConnectionServeur/CConnectionServeur/Source.cpp
@@ -10,10 +10,28 @@ int main(int argc, char* argv[])
         std::cout << " manque 1 argument ( IP / Port ) " << std::endl;
         return 0;
     }
+
+   std::string IP_SERVEUR = argv[1];
+   std::string port = argv[2];
     //--------------------------------------------------------------------------------------------------------------
 
+    CConnectionServeur connection(IP_SERVEUR, port);
+     connection.initConnec();
+    connection.connection();
 
+    std::string message;
+    connection.envoi("Bonjour !");
+    do
+    {
+    
+        message = connection.reception();
+        connection.envoi(message);
+        std::cout << message << std::endl;
+    
+    } while (message != "BYE");
 
+   
+   
 
-    std::cout << "Hello World!\n";
+   
 }
