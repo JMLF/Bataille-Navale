5d7ae0c16d544a29a193a58c80afa15e4f4cf4ce
takerline <takerline@orange.fr>
2021-11-22 08:55:24 +0100

    Ajout Classe CGrille

---
+++ ClientSide/CGrille/CGrille/CGrille.cpp
@@ -0,0 +1,31 @@
+
+
+#include "CGrille.h"
+
+
+CGrille::CGrille()
+{
+}
+
+CGrille::Case CGrille::getCase(int ligne, std::string collone)
+{
+    return Case();
+}
+
+void CGrille::setCase(int ligne, std::string collone, Case type)
+{
+}
+
+void CGrille::afficherGrille()
+{
+}
+
+std::vector<std::string> CGrille::saisieJoueur()
+{
+    return std::vector<std::string>();
+}
+
+bool CGrille::partiePerdue()
+{
+    return false;
+}
