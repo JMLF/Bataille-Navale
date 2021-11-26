71901416534f2aa340dac813cff6d706e7e21e24
JMLF <49654973+JMLF@users.noreply.github.com>
2021-11-26 11:33:22 +0100

    correctif premier echange sur le client

--- ClientSide/ClientBatailleNavale/ClientBatailleNavale/ClientBatailleNavale.cpp
+++ ClientSide/ClientBatailleNavale/ClientBatailleNavale/ClientBatailleNavale.cpp
@@ -29,13 +29,14 @@ int main(int argc, char* argv[])
     Sleep(10);
     grille.placerBateau();
     message = connection.reception(); //reception du message qui dit qu'on peux parler ou que l'autre joueur place ses bateau
-    std::cout << message << std::endl;
+   
 
 
     if (message == "Serveur: votre tour de jouer") //on previent le joueur 2 que l'on a fini de placer les bateaux 
     {
-        connection.envoi("joueur: J'ai ini de placer mes bateaux"); 
 
+        connection.envoi("joueur: J'ai ini de placer mes bateaux"); 
+        
         do
         {
             //std::cout << "on entre dans la boucle 1" << std::endl; pour debug
@@ -56,6 +57,7 @@ int main(int argc, char* argv[])
     }
     if (message == "joueur: J'ai ini de placer mes bateaux") //si on reçoit le message que le joueur 2 a fini on peux envoyer une trame puisque c'est notre tour coté serveur
     {
+        std::cout << message << std::endl;
         do
         {
           //  std::cout << "on entre dans la boucle 2" << std::endl; pour debug
