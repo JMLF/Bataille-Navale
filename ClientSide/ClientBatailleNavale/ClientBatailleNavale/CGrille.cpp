8e41b16dbea1691281ce6ab29c3359b5957d40b3
takerline <takerline@orange.fr>
2021-12-06 11:10:56 +0100

    Taille changer a 9

--- ClientSide/ClientBatailleNavale/ClientBatailleNavale/CGrille.cpp
+++ ClientSide/ClientBatailleNavale/ClientBatailleNavale/CGrille.cpp
@@ -13,8 +13,8 @@ CGrille::CGrille()
     nbCroiseur = 1;
     nbTorpilleur = 0;
     nbSousMarin = 0;
-    colonne = 10;
-    ligne = 10;
+    colonne = TAILLEGRILLE - 1;
+    ligne = TAILLEGRILLE - 1;
     //grille[9][9] = Case::VIDE;
     for (int i = 0; i < ligne ; i++) {
         for (int f = 0; f < colonne ; f++) {
